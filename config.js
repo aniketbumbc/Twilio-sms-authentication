3900042b45392a7b7c5cd6edb0bacc308c75f29e
Aniket B <aniket.umbc@gmail.com>
2020-06-23 21:39:44 +0530

    remove code

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    serviceID:'VA8b77cc07a8cf19b49de31abe590b55d6',
-    accountSID:'ACc2e07d92595462eda49fe875937892e0',
-    authToken:'1cf41d6209f2a8339efad38cd26083c0'
+    serviceID:'',
+    accountSID:'',
+    authToken:''
 }
 //set in envirmental variable 
